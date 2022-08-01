b032420fc092229465b4d9da2968a03f5a879377
khoder habbal <khoderhabbal36@gmail.com>
2022-08-01 13:03:14 +0300

    Crete-Context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './Store';
 import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-    <App />
-    </HelmetProvider>
+  <StoreProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
 
