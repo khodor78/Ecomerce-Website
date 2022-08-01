559ec5892b03acae13e0c78b1c8bc50d3368b213
khoder habbal <khoderhabbal36@gmail.com>
2022-08-01 15:54:57 +0300

    ADD_TO_CART

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import { LinkContainer } from 'react-router-bootstrap';
+import CartScreen from './screens/CartScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -25,7 +26,7 @@ function App() {
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                      {cart.cartItems.length}
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
@@ -38,6 +39,7 @@ function App() {
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen />} />
           <Route path="/" element={<HomeScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
         </Routes>
         </Container>
       </main>
